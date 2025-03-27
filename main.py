cc0788aadbf67a1fd40b21e06332bb3b07891457
wesle-yey <clashcomifeeh@gmail.com>
2025-03-27 11:00:49 -0300

    API Middleware para verificar se o usuário está logado

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from app.routes import alunos, cursos, professores, auth
-from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-templates = Jinja2Templates(directory="templates")
 
 # Inclui os módulos de rotas
 app.include_router(alunos.router)
 app.include_router(cursos.router)
 app.include_router(auth.router)
 app.include_router(professores.router)
+
+#atuar no meio da requisição, para verificar se o usuário está logado
+@app.middleware('http')
+async def middleware_login(request: Request, call_next):
+    response = await call_next(request)
+    
+     # Caso usuário não esteja logado, redireciona para a página de login
+    if response.status_code == 401:
+        print("Requisição não autorizada (401)")
+        return RedirectResponse("/login")
+    
+    return response
 
 @app.get("/")
 def home():
