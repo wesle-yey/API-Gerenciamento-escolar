47678fee06e7c9b0a34c9029945833c2f84c30cc
wesle-yey <clashcomifeeh@gmail.com>
2025-04-09 14:08:48 -0300

    docker rodando

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from app.routes import alunos, cursos, professores, auth
+from database.database import create_tables  # Import create_tables
 
 app = FastAPI()
 
-# Inclui os módulos de rotas
+# Create database tables on startup
+create_tables()
+
+# Include routers
 app.include_router(alunos.router)
 app.include_router(cursos.router)
 app.include_router(auth.router)
 app.include_router(professores.router)
 
-#atuar no meio da requisição, para verificar se o usuário está logado
+# Middleware
 @app.middleware('http')
 async def middleware_login(request: Request, call_next):
     response = await call_next(request)
-    
-     # Caso usuário não esteja logado, redireciona para a página de login
     if response.status_code == 401:
         print("Requisição não autorizada (401)")
         return RedirectResponse("/login")
